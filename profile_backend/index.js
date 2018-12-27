561c128965bf84ea4501afbd2068a56a028fd488
Andy william <andy.william@bit-okutama.jp>
2018-12-27 22:55:05 +0900

    Setup env and make route and controller file

--- profile_backend/index.js
+++ profile_backend/index.js
@@ -1,3 +1,6 @@
+// be able to use .env file
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -14,6 +17,27 @@ app.use(cors());
 // Set static  folder for route
 require('./routes/users.route')(app);
 
-const port = process.env.PORT || 5000;
 
-app.listen(port , () => console.log("Server started on port " + port));
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/client/dist/index.html'))
+});
+
+app.on("error", function(err) {
+  console.log(err);
+})
+
+// Set port
+app.set('port', (process.env.PORT || 3000));
+
+// Check if it is connected
+app.listen(app.get('port'), function() {
+	console.log('Server started at port: '+ app.get('port'));
+}).on('error', (err) => {
+  console.log('On error handler');
+  console.log(err);
+});
+
+process.on('uncaughtException', (err) => {
+  console.log('Process.on handler');
+  console.log(err);
+})
